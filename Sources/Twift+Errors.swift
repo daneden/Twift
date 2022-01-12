14d8ffe6991d48ee13d1678477fee3a54246dfe4
Daniel Eden <dan.eden@me.com>
2022-01-12 14:01:24 +0000

    Add localizedDescription

--- Sources/Twift+Errors.swift
+++ Sources/Twift+Errors.swift
@@ -1,26 +1,30 @@
 import Foundation
 
-extension Twift {
-  enum TwiftError: Error {
-    case CallbackURLError
-    case MissingCredentialsError
-    case DecodingError(type: Any.Type, data: Data)
-    case MalformedUserIDError(_ malformedId: String)
-    case UserNotFoundError(_ userId: UserID)
-    
-    var description: String {
-      switch self {
-      case .CallbackURLError:
-        return "The provided callback URL is invalid"
-      case .MissingCredentialsError:
-        return "This API method requires user credentials, but no user credentials were found in this Twift instance"
-      case .DecodingError(let type, let data):
-        return "There was an error decoding the data into the expected type (\(type.self): \(data.description)"
-      case .MalformedUserIDError(let malformedID):
-        return "The user ID \(malformedID) is invalid; it should be an integer represented as a String"
-      case .UserNotFoundError(let userId):
-        return "No user with id \(userId) found"
-      }
+
+enum TwiftError: Error {
+  case CallbackURLError
+  case MissingCredentialsError
+  case DecodingError(type: Any.Type, data: Data)
+  case MalformedUserIDError(_ malformedId: String)
+  case UserNotFoundError(_ userId: UserID)
+  
+  var description: String {
+    switch self {
+    case .CallbackURLError:
+      return "The provided callback URL is invalid"
+    case .MissingCredentialsError:
+      return "This API method requires user credentials, but no user credentials were found in this Twift instance"
+    case .DecodingError(let type, let data):
+      return "There was an error decoding the data into the expected type (\(type.self): \(data.description)"
+    case .MalformedUserIDError(let malformedID):
+      return "The user ID \(malformedID) is invalid; it should be an integer represented as a String"
+    case .UserNotFoundError(let userId):
+      return "No user with id \(userId) found"
     }
   }
+  
+  var localizedDescription: String {
+    return description
+  }
 }
+
