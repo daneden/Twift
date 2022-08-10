3465b39406319eaa866c6050532251d91aa35bbe
Daniel Eden <dan.eden@me.com>
2022-08-10 10:03:17 +0100

    Make changes to Twift.authenticationType trigger objectDidChange and call refresh token callback even when the token has not changed

--- Sources/Twift.swift
+++ Sources/Twift.swift
@@ -4,7 +4,7 @@ import Combine
 @MainActor
 public class Twift: NSObject, ObservableObject {
   /// The type of authentication access for this Twift instance
-  public private(set) var authenticationType: AuthenticationType
+  @Published public private(set) var authenticationType: AuthenticationType
   public var oauthUser: OAuth2User? {
     switch authenticationType {
       case .oauth2UserAuth(let user, _):
@@ -104,6 +104,7 @@ public class Twift: NSObject, ObservableObject {
     
     // Return early if the token has not yet expired
     if onlyIfExpired && !oauthUser.expired {
+      refreshCompletion?(oauthUser)
       return
     }
     
@@ -131,9 +132,7 @@ public class Twift: NSObject, ObservableObject {
     var refreshedOAuthUser = try JSONDecoder().decode(OAuth2User.self, from: data)
     refreshedOAuthUser.clientId = clientId
     
-    if let refreshCompletion = refreshCompletion {
-      refreshCompletion(refreshedOAuthUser)
-    }
+    refreshCompletion?(refreshedOAuthUser)
     
     self.authenticationType = .oauth2UserAuth(refreshedOAuthUser, onRefresh: refreshCompletion)
   }
